5e1f8048e919dc837abc21a21dafbbfa57fc0d34
JangWonSeok <ja8685@naver.com>
2018-10-13 22:44:47 +0900

    두번째 커밋

--- ch3/polls/urls.py
+++ ch3/polls/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from polls import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', views.index, name='index'),
-    path('polls/<int:question_id>/', views.detail, name='detail'),
-    path('polls/<int:question_id>/results/', views.results, name='results'),
-    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
