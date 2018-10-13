de634a11ca5d4aea603c6609c213e240857be5c7
JangWonSeok <ja8685@naver.com>
2018-10-13 22:31:30 +0900

    첫번째 커밋

--- ch3/mysite/settings.py
+++ ch3/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#uf_qzg!h^p9(_fa=9^hr&t@-y_@9(=e&lm2sj2n6piwg49v19'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
